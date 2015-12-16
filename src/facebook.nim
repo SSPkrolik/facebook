0a804984153fb9379a6d47dcc33cb9937a72f1c7
Rostyslav Dzinko <rostislav.dzinko@gmail.com>
2015-12-16 18:35:30 +0200

    Added login button builder for web FB client

--- src/facebook.nim
+++ src/facebook.nim
@@ -2,11 +2,29 @@
 
 import dom
 import times
+import strutils
 
 const
-  SKUnknown*       : cstring = "unknown"
-  SKNotAuthorized* : cstring = "not_authorized"
-  SKConnected*     : cstring = "connected"
+  SKUnknown*:       string = "unknown"
+  SKNotAuthorized*: string = "not_authorized"
+  SKConnected*:     string = "connected"
+
+const
+  LoginButtonSmall*:  string = "small"
+  LoginButtonMedium*: string = "medium"
+  LoginButtonLarge*:  string = "large"
+  LoginButtonXlarge*: string = "xlarge"
+
+const
+  AudienceEveryone*: string = "everyone"
+  AudienceFriends*:  string = "friends"
+  AudienceOnlyMe*:   string = "only_me"
+
+const
+  FacebookVersion22*: string = "v2.2"
+  FacebookVersion23*: string = "v2.3"
+  FacebookVersion24*: string = "v2.4"
+  FacebookVersion25*: string = "v2.5"
 
 type
   AuthResponse* {.importc.} = ref object
@@ -16,8 +34,8 @@ type
     userID*        {.importc.}: cstring ## Client's user ID
 
   LoginStatus* {.importc.} = ref object
-    status* {.importc.}: cstring
-    authResponse*: AuthResponse
+    status*       {.importc.}: cstring
+    authResponse* {.importc.}: AuthResponse
 
   FacebookInfo* {.importc.} = ref object of RootObj
     ## Facebook Client
@@ -27,9 +45,10 @@ type
     version* {.importc.}: cstring
 
 # `FB` object binding
-var FB* {.importc, nodecl.}: ref RootObj
-proc init*(fb: ref object, info: FacebookInfo) {.importc, nodecl.}
-proc getLoginStatus*(fb: ref object, callback: proc(response: LoginStatus)) {.importc, nodecl.}
+var FB {.importc, nodecl.}: ref RootObj
+proc init(fb: ref object, info: FacebookInfo) {.importc, nodecl.}
+proc getLoginStatus(fb: ref object, callback: proc(response: LoginStatus)) {.importc, nodecl.}
+proc logout(fb: ref object, callback: proc(response: LoginStatus)) {.importc, nodecl.}
 
 let nim_fb_load_api_async = proc(d: Document, s: string, id: string) =
   ## Asynchronous Facebook SDK loading proc
@@ -60,3 +79,18 @@ proc getLoginStatus*(onLoggedIn: proc(response: LoginStatus)) =
   {.emit: """
   FB.getLoginStatus(`onLoggedIn`);
   """.}
+
+proc logout*() =
+  FB.logout(proc(response: ref object) = discard)
+
+proc loginUI*(onLogin: proc(), autoLogoutLink: bool = true, maxRows: int = 1, scope: seq[string] = @["public_profile"], size: string = LoginButtonSmall, showFaces: bool = false, defaultAudience: string = "friends"): Element =
+  ## Build Login UI - "blue button"
+  let scopeStr = join(scope, ",")
+  var btn = document.createElement("fb:login-button")
+  btn.setAttribute("auto-logout-link", $autoLogoutLink)
+  btn.setAttribute("max-rows", $maxRows)
+  btn.setAttribute("scope", scopeStr)
+  btn.setAttribute("size", size.cstring)
+  btn.setAttribute("show-faces", $showFaces)
+  btn.setAttribute("default-audience", defaultAudience.cstring)
+  return btn
